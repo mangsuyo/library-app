ac5c05ff9e19835132fac6e54107e77c0ab8dbc4
mangsuyo <mangsuyo3610@gmail.com>
2024-03-31 17:53:54 +0900

    Feat : 유저 생성 기능

---
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -0,0 +1,19 @@
+package com.group.libraryapp.domain.user;
+
+public class User {
+    String name;
+    Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
---
+++ src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -0,0 +1,14 @@
+package com.group.libraryapp.dto.user.request;
+
+public class UserCreateRequest {
+    String name;
+    Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
