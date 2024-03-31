e459741af526806a8e236c7352ddbb4947abbee0
mangsuyo <mangsuyo3610@gmail.com>
2024-03-31 17:54:59 +0900

    Refactor : 유저 등록 함수 이름 변경
    user -> createUser

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,6 +2,7 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,8 @@ public class UserController {
     List<User> users = new ArrayList<>();
 
     @PostMapping("/user")
-    public void user(@RequestBody UserCreateRequest userCreateRequest){
+    public void createUser(@RequestBody UserCreateRequest userCreateRequest){
         users.add(new User(userCreateRequest.getName(), userCreateRequest.getAge()));
     }
+    
 }
